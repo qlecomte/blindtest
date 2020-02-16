44f37dace0340a47b55059bec623b63f081154d7
Quentin Lecomte <quentinh.lecomte@gmail.com>
2020-02-16 20:53:39 +0100

    Add Mocha Framework

---
+++ tests/example.test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('Basic Mocha String Test', function () {
+    it('should return 2+2 = 4', function () {
+        expect(2+2).to.equal(4)
+    })
+})
